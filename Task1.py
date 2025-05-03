255e15ad7a76d5f9564b4646326b827f51a74185
Kr Vikky <127074056+kmrvikky@users.noreply.github.com>
2025-05-03 22:23:02 +0530

    Module 2: Basic Python Concepts
    Python course - Module 2: Basic Python Concepts for additional guidelines and examples

---
+++ Task1.py
@@ -0,0 +1,22 @@
+# Write a Python program that does the following:
+# 1.  Takes two numbers as input from the user.
+# 2.  Performs the basic mathematical operations on these two numbers:
+# o	Addition
+# o	Subtraction
+# o	Multiplication
+# o	Division
+# 3.  Displays the results of each operation on the screen.
+
+
+a = int(input("Enter the first number: "))
+b = int(input("Enter the second number: "))
+
+Addition = a + b
+Subtraction = a - b
+Multiplication = a * b
+Division = a / b
+
+print("Addition:", Addition)
+print("Subtraction:", Subtraction)
+print("Multiplication:", Multiplication)
+print("Division:", Division)
