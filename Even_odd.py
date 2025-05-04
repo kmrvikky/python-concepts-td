72cb3a971cc89b2da9c5407dbcfca60a40163780
Kr Vikky <127074056+kmrvikky@users.noreply.github.com>
2025-05-04 18:33:48 +0530

    M3.py
    # Python course - Module 3: Control Structures in Python for additional guidelines and examples

---
+++ Even_odd.py
@@ -0,0 +1,11 @@
+# task1_even_odd.py
+
+# Step 1: Take integer input from the user
+# Step 2: Check if it's even or odd
+
+n = int(input("Enter a number: "))
+if n % 2 == 0:
+    print(f"{n} is even.")
+else:
+    print(f"{n} is odd.")
+
